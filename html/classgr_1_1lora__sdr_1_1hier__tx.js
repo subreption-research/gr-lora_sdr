ed84870e0d7647cfc02a956a9c92295191b847b3
GitHub Action <action@github.com>
2021-01-05 13:33:59 +0100

    Automatically updated the wiki

--- html/classgr_1_1lora__sdr_1_1hier__tx.js
+++ html/classgr_1_1lora__sdr_1_1hier__tx.js
@@ -1,5 +1,5 @@
 var classgr_1_1lora__sdr_1_1hier__tx =
 [
     [ "sptr", "classgr_1_1lora__sdr_1_1hier__tx.html#afba19347a5f154e44cb3eff785075114", null ],
-    [ "make", "classgr_1_1lora__sdr_1_1hier__tx.html#a05288d47f92073c84086ea28cdd292a6", null ]
+    [ "make", "classgr_1_1lora__sdr_1_1hier__tx.html#a94ed37f5294e2fe7ccacdea444a0ced0", null ]
 ];
--- html/classgr_1_1lora__sdr_1_1hier__tx__impl.js
+++ html/classgr_1_1lora__sdr_1_1hier__tx__impl.js
@@ -1,5 +1,5 @@
 var classgr_1_1lora__sdr_1_1hier__tx__impl =
 [
-    [ "hier_tx_impl", "classgr_1_1lora__sdr_1_1hier__tx__impl.html#a2aee0666caf8dc0e386063990f0a0e68", null ],
+    [ "hier_tx_impl", "classgr_1_1lora__sdr_1_1hier__tx__impl.html#aeed8f28ff962977a845d9ec64a2f6fa3", null ],
     [ "~hier_tx_impl", "classgr_1_1lora__sdr_1_1hier__tx__impl.html#a721552f3561d65c45d2029d60e057899", null ]
 ];
